acd6f7f59e7d551a76350ef794c47e7f20255b05
DimaPekutko <dkefir03@gmail.com>
2021-05-05 18:49:18 +0300

    updating port for release

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const path = require('path');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 const { exec } = require("child_process");
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname + '/public')));
 
